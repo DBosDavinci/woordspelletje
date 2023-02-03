a80bccc70fab8649ec6cf97ce487141ac6063523
DBosDavinci <99069008@mydavinci.nl>
2023-02-03 09:46:57 +0100

    letters

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
 import random
 
+def KiesWoord(woorden):
+    woord = random.choice(woorden)
+    return woord
+
+def VraagLetter():
+    letter = input("Kies 1 letter")
+    if len(letter) > 1:
+        print("Je kan maar 1 letter tegelijk kiezen")
+    else:
+        return letter
+
 try:
     with open("woorden_eenvoudig.txt", "r") as file:
         content = file.read()
@@ -7,7 +18,16 @@ try:
 except:
     print('error with opening file')
 
-print(random.choice(woordenlijst))
+woord = KiesWoord(woordenlijst)
+letters = list(woord)
+print(letters)
+correctletters = ""
 
-def KiesWoord(woordenlijst:list):
-    random.choice(woordenlijst)
+while True:
+    letter = VraagLetter()
+    for x in letters:
+        if letter == x:
+            correctletters+=letter
+        else:
+            continue
+    print(correctletters)
